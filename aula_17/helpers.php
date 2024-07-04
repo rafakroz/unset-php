00fa9634a72f4d2565275716b428ed09ae0a279d
Rafael Trabalho <rafakroz@gmail.com>
2024-07-04 16:13:42 -0300

    Arquivos da aula 17

---
+++ aula_17/helpers.php
@@ -0,0 +1,11 @@
+<?php
+
+function saudacao($texto) : string
+{
+    return $texto;
+}
+
+function resumirTexto (string $texto, int $limite, string $continue = '...'): string
+{
+    return $texto;
+}
