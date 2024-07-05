455f408c602a03d249d58ea596fcbdb841692559
Rafael Trabalho <rafakroz@gmail.com>
2024-07-05 08:36:53 -0300

    Arquivos da aula 19

---
+++ aula_19/index.php
@@ -0,0 +1,21 @@
+<?php
+
+include_once 'helpers.php';
+
+/* Curso de PHP 8 Aula 018 Estrutura de Controle IF */
+
+$texto = 'Teste do index';
+
+echo saudacao();
+
+echo '<br><br>';
+
+/*----------------------------------*/
+
+/*
+echo 'Vardump:' . '<br><br>';
+
+echo 'Vardump string: <br>';
+var_dump($texto);
+echo '<br><br>';
+*/
