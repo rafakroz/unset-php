5155f36d0b2074b88c9a6960af5bca9c7213a62f
Rafael Trabalho <rafakroz@gmail.com>
2024-07-09 16:58:05 -0300

    Arquivos das aulas 37 e 38

---
+++ aulas_30_39/aula_38/index.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'sistema/configuracao.php';
+include_once 'helpers.php';
+
+echo "<b>Aula 038 Introdução as Expressões Regulares</b>";
+echo '<hr><hr>';
+
+$cpf = '004.348.491-38';
+$cpf2 = '00434849138';
+
+/*
+preg_replace: efetua subsituição de string com definição de expressão regular
+*/
+
+echo '$cpf: <br>';
+var_dump(validarCpf($cpf));
+
+echo '<br><br>';
+
+echo '$cpf2: <br>';
+var_dump(validarCpf($cpf2));
+
+//echo $limparNumero = preg_replace('/[^0-9]/', '', $cpf);
