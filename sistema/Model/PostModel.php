66a7526279cb3272d7cd7055c924e26109c7da94
Rafael Trabalho <rafakroz@gmail.com>
2024-07-16 15:19:40 -0300

    Aula 71. Selecionando dados com Where

--- sistema/Model/PostModel.php
+++ sistema/Model/PostModel.php
@@ -6,9 +6,16 @@ use sistema\Nucleo\Connection;
 
 class PostModel
 {
-    public function ler(): array
+    public function ler(int|null $id = null): array
     {
-        $query = "SELECT * FROM posts";
+        $where = ($id ? "WHERE id = {$id}" : '');
+
+        $query = "SELECT * FROM posts {$where}";
+        //$query = "SELECT * FROM posts WHERE title = 'titulo do post 3'";
+        //$query = "SELECT * FROM posts LIMIT 2, 2";
+        //$query = "SELECT * FROM posts LIMIT 2 OFFSET 5";
+        //$query = "SELECT * FROM posts LIMIT 2";
+        //$query = "SELECT * FROM posts WHERE id = 3 AND status = 1 OR status = 0";
 
         $stmt = Connection::getInstancia()->query($query);
 
