9de82fe540df44055c216b0ea0dfe2e5b24a8ed2
Rafael Trabalho <rafakroz@gmail.com>
2024-07-17 08:47:00 -0300

    Aula 72. Teste de queries

--- sistema/Model/PostModel.php
+++ sistema/Model/PostModel.php
@@ -11,11 +11,14 @@ class PostModel
         $where = ($id ? "WHERE id = {$id}" : '');
 
         $query = "SELECT * FROM posts {$where}";
-        //$query = "SELECT * FROM posts WHERE title = 'titulo do post 3'";
-        //$query = "SELECT * FROM posts LIMIT 2, 2";
-        //$query = "SELECT * FROM posts LIMIT 2 OFFSET 5";
-        //$query = "SELECT * FROM posts LIMIT 2";
-        //$query = "SELECT * FROM posts WHERE id = 3 AND status = 1 OR status = 0";
+        // $query = "SELECT * FROM posts WHERE title = 'titulo do post 3'";
+        // $query = "SELECT * FROM posts LIMIT 2, 2";
+        // $query = "SELECT * FROM posts LIMIT 2 OFFSET 5";
+        // $query = "SELECT * FROM posts LIMIT 2";
+        // $query = "SELECT * FROM posts WHERE id = 2 AND (status = 1 OR status = 0)";
+        // $query = "SELECT * FROM posts WHERE id = 4 AND status in (0)";
+        // $query = "SELECT * FROM posts WHERE status in (1)";
+        // $query = "SELECT * FROM posts WHERE id = 3 AND (status = 0 OR status = 1)";
 
         $stmt = Connection::getInstancia()->query($query);
 
